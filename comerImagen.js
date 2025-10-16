614f9f792601af3d62a4a3ece547a6b07d73b56a
Arturo <arturo@abastos.org>
2025-10-16 13:46:05 +0000

    añadida página web

---
+++ comerImagen.js
@@ -0,0 +1,7 @@
+const saludo = document.getElementById("saludo");
+const mensaje = document.getElementById("mensaje");
+
+saludo.addEventListener("click", () => {
+    saludo.style.opacity = "0";
+    mensaje.textContent = "Noooooooooooooooooooooo";
+});
